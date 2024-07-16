16b099bf96f9afc2c37cd0f81345f4adf6e3c47a
MaxvG115 <maxivillegas115@gmail.com>
2024-07-16 17:57:34 -0600

    Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="w-screen h-screen bg-gray-900 flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">My first journal app:)</h1>
+        <p className="text-2xl text-gray-400 mb-4">This is an app that tracks your mood throught your life, just be honest</p>
+        <div>
+          <Link href={"/journal"}>
+            <button className="bg-cyan-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
