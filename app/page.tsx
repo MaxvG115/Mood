f10a69c4d26dfe658264afc9f955a4f2b98d584b
MaxvG115 <maxivillegas115@gmail.com>
2024-07-17 17:26:33 -0600

    Revert "Auth final details"
    This reverts commit 9d022d2ad65f87de7ece36ad891c0037a63a3351.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import Link from "next/link";
-import { auth } from "@clerk/nextjs/server";
 
-export default async function Home() {
-  const {userId} = await auth()
-  let href = userId ? '/journal' : '/new-user'
-
+export default function Home() {
   return (
     <div className="w-screen h-screen bg-gray-900 flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
         <h1 className="text-6xl mb-4">My first journal app:)</h1>
         <p className="text-2xl text-gray-400 mb-4">This is an app that tracks your mood throught your life, just be honest</p>
         <div>
-          <Link href={href}>
+          <Link href={"/journal"}>
             <button className="bg-cyan-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
           </Link>
         </div>
