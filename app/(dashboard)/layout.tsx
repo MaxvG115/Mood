41f9b9b1e3cf01458ef20dc33dc77ca5a34413d6
MaxvG115 <maxivillegas115@gmail.com>
2024-07-17 18:16:36 -0600

    User profile widget

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,20 @@
+import { UserButton } from "@clerk/nextjs";
+import React from 'react';
+
 const dashboardLayout = ({ children }) => {
     return(
-        <div>
-            {children}
+        <div className="h-screen w-screen relative">
+            <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/20">
+                Mood
+            </aside>
+            <div className="ml-[200px]">
+                <header className="h-[60px] border-b border-black/20">
+                    <div className="h-full w-full px-6 flex items-center justify-end">
+                        <UserButton />
+                    </div>
+                </header>
+                <div>{children}</div>
+            </div>
         </div>
     )
 }
